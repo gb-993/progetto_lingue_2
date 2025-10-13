8d51393b9a447031982681cdb5815cc5c687bf0d
gb-993 <gbmatteazzi93@gmail.com>
2025-10-13 18:21:04 +0200

    eliminato staticfile da settings

--- progetto_lingua_2/settings.py
+++ progetto_lingua_2/settings.py
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "django.contrib.postgres",
 
     # Project apps
