b9a149a1eb30cabc3b2e421f0e9e2e17d6c37036
gb-993 <gbmatteazzi93@gmail.com>
2025-10-13 18:15:16 +0200

    sistemati flash messages

---
+++ static/js/messages.js
@@ -0,0 +1,13 @@
+// static/js/messages.js
+(function(){
+  document.addEventListener('click', function(e){
+    if (e.target.matches('.alert-close')) {
+      e.target.closest('.alert')?.remove();
+    }
+  });
+
+  document.querySelectorAll('.alert-success, .alert-info').forEach(function(el){
+    const timer = setTimeout(() => el.remove(), 6000);
+    el.addEventListener('mouseenter', () => clearTimeout(timer), { once: true });
+  });
+})();
